2c0080083c5a19208880f2f7eb99807a5d0c0a81
Farina Salman <farinasalman@gmail.com>
2021-05-24 23:47:11 -0400

    worked on styling

--- src/App.js
+++ src/App.js
@@ -12,20 +12,23 @@ function App() {
         <img src={outstem_logo} className="outstem-logo" alt="Outstem Logo"/>
       </div>
       <div className="sign-in">
-        <h1>Sign up to begin your adventure</h1>
+        <h2>Sign up to begin your adventure</h2>
         <p>Already a member? <a href="">Login instead</a></p>
-        <button className="google-button"><img src={google_logo} alt="Google logo"/>Continue with Google</button>
+        <button className="google-button"><img src={google_logo} alt="Google logo" width="20"/>Continue with Google</button>
         <p>or sign up with email</p>
         <form className="sign-in-email">
           <label htmlFor="email">Email</label>
+          <br></br>
           <input type="text" name="email" id="email"/>
+          <br></br>
           <label htmlFor="password">Password (min. 8 characters)</label>
+          <br></br>
           <input type="text" name="password" id="password"/>
           <div className="checkbox">
             <input type="checkbox" name="terms-privacy" id="terms-privacy"/>
             <label htmlFor="terms-privacy">I agree to the <a href="">Terms of Service</a> and <a href="">Privacy Policy</a></label>
-            <button type="submit" className="submit">Get Started</button>
           </div>
+          <button type="submit" className="submit">Get Started</button>
         </form>
       </div>
     </div> 
