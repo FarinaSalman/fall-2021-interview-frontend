c27ac9516174fbb2f69c115a2fa4fd1adb38486e
Farina Salman <farinasalman@gmail.com>
2021-05-24 22:19:30 -0400

    added html components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import backdrop from './assets/backdrop.jpg';
+import google_logo from './assets/google_logo.svg';
+import outstem_logo from './assets/outstem_logo_icon.png';
 
 function App() {
-  const name = 'Farina'
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-    </div>
+      <div className="image">
+        <img src={backdrop} className="backdrop-img" alt="backdrop"/>
+        <img src={outstem_logo} className="outstem-logo" alt="Outstem Logo"/>
+      </div>
+      <div className="sign-in">
+        <h1>Sign up to begin your adventure</h1>
+        <p>Already a member? <a href="">Login instead</a></p>
+        <button className="google-button"><img src={google_logo} alt="Google logo"/>Continue with Google</button>
+        <p>or sign up with email</p>
+        <form className="sign-in-email">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email"/>
+          <label htmlFor="password">Password (min. 8 characters)</label>
+          <input type="text" name="password" id="password"/>
+          <div className="checkbox">
+            <input type="checkbox" name="terms-privacy" id="terms-privacy"/>
+            <label htmlFor="terms-privacy">I agree to the <a href="">Terms of Service</a> and <a href="">Privacy Policy</a></label>
+            <button type="submit" className="submit">Get Started</button>
+          </div>
+        </form>
+      </div>
+    </div> 
   );
 }
 
